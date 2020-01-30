ea9e953882dab123f6e2b7257e1b746cb4546b50
alexiliev <iliev.alex@outlook.com>
2020-01-29 16:29:18 -0800

    refactored to move posts into components

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,50 @@
+<template>
+    <nuxt-link :to="  '/posts/'+1 " class="post-preview">
+      <article >
+        <div class="post-thumbnail" style="background-image: url(https://images.unsplash.com/photo-1577457674844-da86606d2de6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)"></div>
+        <div class="post-content">
+          <h1>Post title</h1>
+          <p>some preview text</p>
+        </div>
+        </article>
+    </nuxt-link>
+</template>
+
+<style scoped>
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,28 +4,26 @@
     <h1>some headline for front page</h1>
   </section>
   <section class="featured-posts">
-    <nuxt-link :to="  '/posts/'+1 " class="post-preview">
-      <article >
-        <div class="post-thumbnail" style="background-image: url(https://images.unsplash.com/photo-1577457674844-da86606d2de6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)"></div>
-        <div class="post-content">
-          <h1>Post title</h1>
-          <p>some preview text</p>
-        </div>
-      </article>
-    </nuxt-link>
-    <nuxt-link :to="  '/posts/'+2 " class="post-preview">
-      <article >
-        <div class="post-thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572880393162-0518ac760495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80)"></div>
-        <div class="post-content">
-          <h1>Post title two</h1>
-          <p>some preview text two</p>
-        </div>
-      </article>
-    </nuxt-link>
+    <PostPreview />
   </section>
 
 </div>
 </template>
+
+
+
+<script>
+  import PostPreview from '~/components/Posts/PostPreview'
+  export default {
+    components: {
+      PostPreview
+    }
+  }
+</script>
+
+
+
+
 
 <style scoped>
 .intro {
@@ -67,39 +65,4 @@
   justify-content: center;
 }
 
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
-}
 </style>
