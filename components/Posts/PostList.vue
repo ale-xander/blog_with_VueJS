7a2b83a098f0d81d9bcf32daafd591437b32747e
alexiliev <iliev.alex@outlook.com>
2020-01-29 17:47:05 -0800

    made the posts a component and moved everything to PostList

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="posts-page">
+    <section class="post-list">
+    <PostPreview 
+        id="1"
+        thumbnail='https://images.unsplash.com/photo-1577457674844-da86606d2de6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+        title='some turtles'
+        previewText='my first post'
+    />
+    <PostPreview 
+        id="2"
+        thumbnail='https://images.unsplash.com/photo-1574923842719-94d5f9e28793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        title='malo gato'
+        previewText='a cat'
+    />
+    <PostPreview 
+        id="3"
+        thumbnail='https://images.unsplash.com/photo-1532022160153-7735cd049992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2090&q=80'
+        title='perro bueno'
+        previewText='Dalmatian'
+    />
+    </section>
+    </div>
+</template>
+
+<script>
+  import PostPreview from '~/components/Posts/PostPreview'
+  export default {
+    components: {
+      PostPreview
+    }
+  }
+</script>
+
+
+
+<style scoped>
+.posts-page{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,7 @@
   <section class="intro">
     <h1>some headline for front page</h1>
   </section>
-  <section class="featured-posts">
-    <PostPreview 
-      id="1"
-      thumbnail='https://images.unsplash.com/photo-1577457674844-da86606d2de6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
-      title='some turtles'
-      previewText='my first post'
-    />
-    <PostPreview 
-      id="2"
-      thumbnail='https://images.unsplash.com/photo-1574923842719-94d5f9e28793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
-      title='malo gato'
-      previewText='a cat'
-    />
-    <PostPreview 
-      id="1"
-      thumbnail='https://images.unsplash.com/photo-1532022160153-7735cd049992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2090&q=80'
-      title='perro bueno'
-      previewText='Dalmatian'
-    />
-  </section>
+  <PostList />
 
 </div>
 </template>
@@ -30,10 +11,10 @@
 
 
 <script>
-  import PostPreview from '~/components/Posts/PostPreview'
+  import PostList from '~/components/Posts/PostList'
   export default {
     components: {
-      PostPreview
+      PostList
     }
   }
 </script>
